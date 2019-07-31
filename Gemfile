f1866a79fd317d962c0f4cec84bceb8ecf0dd12a
david medina <davidmeddina@gmail.com>
2019-07-30 22:13:11 -0500

    login with facebook using omniauth

--- Gemfile
+++ Gemfile
@@ -27,7 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'devise'
 gem 'haml-rails'
+gem 'omniauth-facebook'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+               omniauth_callbacks: 'users/omniauth_callbacks'
+             }
+  post '/custom_sign_up', to: 'users/omniauth_callbacks#custom_sign_up'
   root 'main#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
